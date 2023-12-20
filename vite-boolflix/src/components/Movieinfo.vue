950e0029744fce18a2eeab9bbcd0f8bbcf2062c5
“Bernardo <bernardobarbucci@gmail.com>
2023-12-20 19:59:36 +0100

    creato template di movieinfo

--- vite-boolflix/src/components/Movieinfo.vue
+++ vite-boolflix/src/components/Movieinfo.vue
@@ -1,6 +1,11 @@
 <template lang="">
     <section class="movie-info">
-        
+        <div v-if="movieDetails">
+            <h2>{{ movieDetails.title }}</h2>
+            <p>Titolo Originale: {{ movieDetails.original_title }}</p>
+            <p>Lingua: {{ movieDetails.original_language }}</p>
+            <p>Voto: {{ movieDetails.vote_average }}</p>
+        </div>
     </section>
 </template>
 
