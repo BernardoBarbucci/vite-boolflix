f92bafab2409acde25080b6ae92389475f87e35a
“Bernardo <bernardobarbucci@gmail.com>
2023-12-21 12:28:24 +0100

    cambio di programma: provo con uno store.js e emit per semplificare gli script individuali nei components

--- vite-boolflix/src/components/Appmain.vue
+++ vite-boolflix/src/components/Appmain.vue
@@ -1,35 +1,45 @@
 <template>
-    <div>
-        <Moviesearch :callAPI="'callAPIForSearch'" />
-    </div>
+    <main>
+        <Moviesearch :callAPI="callAPIForSearch" />
+        <Movieinfo :callAPI="callAPI" :movieID="selectedMovieID" />
+    </main>
 </template>
   
 <script>
 import Moviesearch from './Moviesearch.vue';
+import Movieinfo from './Movieinfo.vue';
 
 export default {
     name: 'Appmain',
     components: {
         Moviesearch,
+        Movieinfo,
+    },
+    data() {
+        return {
+            selectedMovieID: null,
+        };
     },
     methods: {
         // Funzione per chiamate API generiche
-        callAPI(endpoint, params = {}) {
+        callAPI(endpoint, params = {
+
+        }) {
             // Aggiungi qui la tua logica per le chiamate API generiche
         },
         // Funzione specifica per la ricerca di film
         callAPIForSearch(params) {
             return this.callAPI('search/movie', { ...params });
         },
+        // Funzione per visualizzare i dettagli del film
+        showMovieDetails(movieID) {
+            this.selectedMovieID = movieID;
+        },
     },
 };
 </script>
 
-<style lang="scss">
-h1 {
-    color: white;
-}
-</style>
+<style lang="scss"></style>
 
 <!-- TYPE: 
 cd /Users/Berna/Desktop/vite-boolflix/vite-boolflix
