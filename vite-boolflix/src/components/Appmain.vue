57ac8a5edf718ebc44c86c5e59029218e65164f2
“Bernardo <bernardobarbucci@gmail.com>
2023-12-22 11:21:44 +0100

    appmain aggiornato per collegarsi a searchmovies + utilizzo created al posto del mounted

--- vite-boolflix/src/components/Appmain.vue
+++ vite-boolflix/src/components/Appmain.vue
@@ -20,48 +20,64 @@ export default {
     },
     data() {
         return {
-            selectedMovieID: null,
-        };
+
+        }
     },
     methods: {
-        // funzione generica per fare le chiamate api
-        async callAPI(endpoint, params = {}) {
-            try {
-                const response = await callAPI(endpoint, params);
-                return response.data;
-            } catch (error) {
-                console.error('Errore nella chiamata API:', error);
-                throw error;
-            }
-        },
-        // funzione specifica per cercare i film
-        async callAPIForSearch(params) {
-            return this.callAPI('search/movie', { ...params });
-        },
-        // gestisco l'evento showmoviedetails che parte da moviesearch
-        showMovieDetails(movieID) {
-            console.log('evento ricevuto coorettamente dal film numero:', movieID);
-            this.selectedMovieID = movieID;
-        },
-        // gestisco l'evento movieInfoMounted emesso da Movieinfo
-        async handleMovieInfoMounted() {
-            // Chiamare fetchMovieDetails direttamente qui nel componente Appmain
-            await this.fetchMovieDetails();
-        },
-        // fetchMovieDetails spostato qua da Moviesearch
-        async fetchMovieDetails() {
-            if (this.selectedMovieID) {
-                try {
-                    const response = await this.callAPI(`movie/${this.selectedMovieID}`);
-                    // Aggiorna i dettagli del film nel componente Appmain
-                    this.$refs.movieInfo.movieDetails = response.data;
-                } catch (error) {
-                    console.error('Errore nel recupero dei dettagli del film:', error);
-                }
-            }
-        },
+        searchMovies(searchElement) {
+
+        }
     },
+    created() {
+        console.log('created funziona');
+        this.searchMovies();
+    }
+
+
 };
+// data() {
+//     return {
+//         selectedMovieID: null,
+//     };
+// },
+// methods: {
+//     // funzione generica per fare le chiamate api
+//     async callAPI(endpoint, params = {}) {
+//         try {
+//             const response = await callAPI(endpoint, params);
+//             return response.data;
+//         } catch (error) {
+//             console.error('Errore nella chiamata API:', error);
+//             throw error;
+//         }
+//     },
+//     // funzione specifica per cercare i film
+//     async callAPIForSearch(params) {
+//         return this.callAPI('search/movie', { ...params });
+//     },
+//     // gestisco l'evento showmoviedetails che parte da moviesearch
+//     showMovieDetails(movieID) {
+//         console.log('evento ricevuto coorettamente dal film numero:', movieID);
+//         this.selectedMovieID = movieID;
+//     },
+//     // gestisco l'evento movieInfoMounted emesso da Movieinfo
+//     async handleMovieInfoMounted() {
+//         // Chiamare fetchMovieDetails direttamente qui nel componente Appmain
+//         await this.fetchMovieDetails();
+//     },
+//     // fetchMovieDetails spostato qua da Moviesearch
+//     async fetchMovieDetails() {
+//         if (this.selectedMovieID) {
+//             try {
+//                 const response = await this.callAPI(`movie/${this.selectedMovieID}`);
+//                 // Aggiorna i dettagli del film nel componente Appmain
+//                 this.$refs.movieInfo.movieDetails = response.data;
+//             } catch (error) {
+//                 console.error('Errore nel recupero dei dettagli del film:', error);
+//             }
+//         }
+//     },
+// },
 
 </script>
 
